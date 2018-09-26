664f229dbc905fcf5f20425f6d2bb27a0f736e8d
Timafei <33371343+TimaG13@users.noreply.github.com>
2018-09-26 18:16:43 +0300

    dsg

--- chekout-pagу/sass/script.js
+++ chekout-pagу/sass/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-
+    $(window).resize(function(){
+        var windowWidth = $(window).width();
+        if(windowWidth < 770) {
+            $(".mi--f").addClass("collapse");
+        }
+        else {
+            $(".mi--f").removeClass("collapse");
+        }
+    });
 });
